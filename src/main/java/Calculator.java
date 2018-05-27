8c013098ef4d55b704e065a4f6c7ce8701c12b3b
konrad <konrad@example.com>
2018-05-27 17:00:06 +0200

    junit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,9 +1,7 @@
-package main;
-
 public class Calculator {
 
 	public double sum(double a, double b) {
-		return a + b;
+		return a +b;
 	}
 
 	public double subtract(double a, double b) {
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,7 +1,5 @@
-package test;
 
-import main.Calculator;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.After;
 import org.junit.Test;
@@ -22,8 +20,59 @@ public class CalculatorTest {
 	}
 	
 	@Test
-	public void test1() throws Exception {
-		System.out.println("Metoda testowa");
+	public void testDodawania() throws Exception {
+		double a = 2.0;
+		double b = 3.0;
+
+		double c = 4.0;
+		double d = 6.7;
+
+		double suma = classUnderTest.sum(a,b);
+		double suma2 = classUnderTest.sum(c,d);
+		Assert.assertEquals("Bledny wynik sumowania", 5.0, suma, 0.001);
+		Assert.assertEquals("Bledny wynik sumowania", 10.7, suma2, 0.001);
+
 	}
 
+	@Test
+	public void testOdejmowania() throws Exception {
+		double e = 4.0;
+		double f = 4.0;
+
+		double substract = classUnderTest.subtract(e,f);
+
+		Assert.assertEquals("Błędny wynik odejmowania", 0, substract, 0.000001);
+	}
+
+	@Test
+	public void testMnozenia() throws Exception {
+		double g = 6.0;
+		double h = 8.0;
+
+		double multiply = classUnderTest.multiply(g,h);
+
+		Assert.assertEquals("Błedny wynik mnozenia", 48.0, multiply, 0.00001);
+	}
+
+	@Test
+	public void testDzielenia() throws Exception {
+
+		double a = 12.0;
+		double b = 6.0;
+
+		double divide = classUnderTest.divide(a,b);
+
+		Assert.assertEquals("Bledny wynik dzielenia", 2.0, divide, 0.00001);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void dzieleniePrzezZero() {
+
+		double a = 4.0;
+		double b = 0.0;
+
+		double wynik = classUnderTest.divide(a,b);
+
+
+	}
 }
